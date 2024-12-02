4c6e2bf4db1cb957899c7afb0406fc2f18424fb5
nyaaar <nyaaarlathotep@163.com>
2024-12-02 14:34:07 +0800

    day2part2

--- day2/solution.go
+++ day2/solution.go
@@ -39,5 +39,46 @@ func PartOne(input string) string {
 }
 
 func PartTwo(input string) string {
-	return ""
+	count := 0
+	for _, line := range strings.Split(input, "\n") {
+		allNums := strings.Split(line, " ")
+		for j := 0; j < len(allNums); j++ {
+			safe := true
+			nums := make([]string, len(allNums)-1)
+			for i := 0; i < j; i++ {
+				nums[i] = allNums[i]
+			}
+			for i := j + 1; i < len(allNums); i++ {
+				nums[i-1] = allNums[i]
+			}
+
+			one, _ := strconv.Atoi(nums[0])
+			two, _ := strconv.Atoi(nums[1])
+			ascend := two > one
+
+			for i := 0; i < len(nums)-1; i++ {
+				one, _ := strconv.Atoi(nums[i])
+				two, _ := strconv.Atoi(nums[i+1])
+				if two > one && !ascend {
+					safe = false
+					break
+
+				} else if one > two && ascend {
+					safe = false
+					break
+				}
+				if !(math.Abs(float64(one-two)) <= 3 && math.Abs(float64(one-two)) >= 1) {
+					safe = false
+					break
+				}
+			}
+			if safe {
+				count++
+				break
+			}
+		}
+
+	}
+	return strconv.Itoa(count)
+
 }
