7dfaff452569b46c6038997378732ef453ef4406
nyaaar <nyaaarlathotep@163.com>
2024-12-03 14:33:32 +0800

    day3part2 regex, son

--- day3/solution_template.go
+++ day3/solution_template.go
@@ -18,6 +18,25 @@ func PartOne(input string) string {
 }
 
 func PartTwo(input string) string {
-	return ""
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := re.FindAllStringSubmatch(input, -1)
+	res := 0
+	do := true
+	for _, match := range matches {
+		if match[0] == "do()" {
+			do = true
+			continue
+		} else if match[0] == "don't()" {
+			do = false
+			continue
+		}
+		if !do {
+			continue
+		}
+		one, _ := strconv.Atoi(match[1])
+		two, _ := strconv.Atoi(match[2])
+		res = res + one*two
+	}
+	return strconv.Itoa(res)
 
 }
--- day3/solution_test.go
+++ day3/solution_test.go
@@ -15,9 +15,10 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	content, err := os.ReadFile("./test")
-	if err != nil {
-		panic(err)
-	}
+	//content, err := os.ReadFile("./test")
+	//if err != nil {
+	//	panic(err)
+	//}
+	content := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 	fmt.Printf("partTwo res: %s \n", PartTwo(string(content)))
 }
