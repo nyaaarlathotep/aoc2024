19a55cb5bfba47c1591559ac18bd5f6db2ff81a5
nyaaar <nyaaarlathotep@163.com>
2024-12-04 15:35:13 +0800

    day3part1

---
+++ day4/solution_template.go
@@ -0,0 +1,129 @@
+package day4
+
+import (
+	"strconv"
+	"strings"
+)
+
+func PartOne(input string) string {
+	lines := strings.Split(input, "\n")
+	byteMap := make([][]byte, len(lines))
+	for i, l := range lines {
+		byteMap[i] = make([]byte, len(l))
+		for j, b := range []byte(l) {
+			byteMap[i][j] = b
+		}
+	}
+	total := 0
+	for i := range byteMap {
+		for j := range byteMap[0] {
+			if byteMap[i][j] == 'X' {
+				allWords := allDirBytes(byteMap, 4, i, j)
+				count := 0
+				for _, word := range allWords {
+					if equalXmas(word) {
+						count++
+					}
+				}
+
+				total += count
+			}
+		}
+	}
+	return strconv.Itoa(total)
+}
+
+func equalXmas(b []byte) bool {
+	return b[0] == 'X' && b[1] == 'M' && b[2] == 'A' && b[3] == 'S'
+}
+
+func allDirBytes(m [][]byte, targetLen int, i, j int) [][]byte {
+	res := make([][]byte, 0)
+	if i-targetLen+1 >= 0 {
+		up := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			up[n] = m[ti][tj]
+			ti--
+		}
+		res = append(res, up)
+	}
+	if j-targetLen+1 >= 0 {
+		left := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			left[n] = m[ti][tj]
+			tj--
+		}
+		res = append(res, left)
+	}
+	if j-targetLen+1 >= 0 && i-targetLen+1 >= 0 {
+		upLeft := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			upLeft[n] = m[ti][tj]
+			ti--
+			tj--
+		}
+		res = append(res, upLeft)
+	}
+
+	if i+targetLen <= len(m) {
+		down := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			down[n] = m[ti][tj]
+			ti++
+		}
+		res = append(res, down)
+	}
+	if j+targetLen <= len(m) {
+		right := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			right[n] = m[ti][tj]
+			tj++
+		}
+		res = append(res, right)
+	}
+
+	if j+targetLen <= len(m) && i-targetLen+1 >= 0 {
+		upRight := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			upRight[n] = m[ti][tj]
+			tj++
+			ti--
+		}
+		res = append(res, upRight)
+	}
+
+	if j+targetLen <= len(m) && i+targetLen <= len(m) {
+		downRight := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			downRight[n] = m[ti][tj]
+			tj++
+			ti++
+		}
+		res = append(res, downRight)
+	}
+
+	if i+targetLen <= len(m) && j-targetLen+1 >= 0 {
+		downLeft := make([]byte, targetLen)
+		ti, tj := i, j
+		for n := 0; n < targetLen; n++ {
+			downLeft[n] = m[ti][tj]
+			ti++
+			tj--
+		}
+		res = append(res, downLeft)
+	}
+
+	return res
+}
+
+func PartTwo(input string) string {
+	return ""
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"aoc2024/day3"
+	"aoc2024/day4"
 	"fmt"
 	"os"
 )
@@ -10,10 +10,10 @@ import (
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	content, err := os.ReadFile("./day3/input")
+	content, err := os.ReadFile("./day4/input")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("partOne res: %s \n", day3.PartOne(string(content)))
-	fmt.Printf("partTwo res: %s \n", day3.PartTwo(string(content)))
+	fmt.Printf("partOne res: %s \n", day4.PartOne(string(content)))
+	fmt.Printf("partTwo res: %s \n", day4.PartTwo(string(content)))
 }
