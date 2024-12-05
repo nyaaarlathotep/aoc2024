3bbf274b4a1786a0aaa52e7ecf129b020dc07571
nyaaar <nyaaarlathotep@163.com>
2024-12-05 15:19:19 +0800

    day4part2

--- day5/solution_template.go
+++ day5/solution_template.go
@@ -39,7 +39,6 @@ func PartOne(input string) string {
 						pass = false
 						break
 					}
-
 				}
 			}
 			if !pass {
@@ -55,6 +54,61 @@ func PartOne(input string) string {
 }
 
 func PartTwo(input string) string {
-	return ""
+	parts := strings.Split(input, "\n\n")
+	graph := make(map[int][]int)
+	for _, l := range strings.Split(parts[0], "\n") {
+		dir := strings.Split(l, "|")
+		one, _ := strconv.Atoi(dir[0])
+		two, _ := strconv.Atoi(dir[1])
+		if v, ok := graph[one]; ok {
+			graph[one] = append(v, two)
+		} else {
+			graph[one] = []int{two}
+		}
+	}
 
+	total := 0
+	for _, l := range strings.Split(parts[1], "\n") {
+		numsStr := strings.Split(l, ",")
+
+		nums := make([]int, len(numsStr))
+		for i := range numsStr {
+			num, _ := strconv.Atoi(numsStr[i])
+			nums[i] = num
+		}
+
+		pass := true
+		for i := len(nums) - 1; i >= 0; i-- {
+			num := nums[i]
+			if nextNums, ok := graph[num]; ok {
+				for j := 0; j < i; j++ {
+					if slices.Contains(nextNums, nums[j]) {
+						pass = false
+						break
+					}
+				}
+			}
+			if !pass {
+				break
+			}
+		}
+		if !pass {
+			slices.SortFunc(nums, func(a, b int) int {
+				if nextNums, ok := graph[a]; ok {
+					if slices.Contains(nextNums, b) {
+						return -1
+					}
+				}
+				if nextNums, ok := graph[b]; ok {
+					if slices.Contains(nextNums, a) {
+						return 1
+					}
+				}
+				panic("!")
+			})
+
+			total += nums[len(nums)/2]
+		}
+	}
+	return strconv.Itoa(total)
 }
