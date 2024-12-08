719db5841d03502a4358d93436c1b06f0b35283d
nyaaar <nyaaarlathotep@163.com>
2024-12-08 15:34:13 +0800

    day8part2

--- day8/solution_template.go
+++ day8/solution_template.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -76,6 +77,80 @@ func legal(m, n, i, j int) bool {
 	return true
 }
 func PartTwo(input string) string {
-	return ""
+	m := make(map[rune][]pos)
 
+	lines := strings.Split(input, "\n")
+	mapHeight := len(lines)
+	mapLength := len(lines[0])
+	for i, l := range lines {
+		for j, b := range l {
+			if b != '.' {
+				if v, ok := m[b]; ok {
+					m[b] = append(v, pos{
+						name: b,
+						i:    i,
+						j:    j,
+					})
+				} else {
+					m[b] = []pos{{
+						name: b,
+						i:    i,
+						j:    j,
+					}}
+				}
+			}
+		}
+	}
+
+	uniquePoses := make(map[pos]bool)
+	for _, v := range m {
+		for i := 0; i < len(v); i++ {
+			one := v[i]
+			for j := i + 1; j < len(v); j++ {
+				two := v[j]
+				iDis := two.i - one.i
+				jDis := two.j - one.j
+				iStart := one.i
+				jStart := one.j
+				for legal(mapHeight, mapLength, iStart, jStart) {
+					uniquePoses[pos{
+						i: iStart,
+						j: jStart,
+					}] = true
+					iStart = iStart + iDis
+					jStart = jStart + jDis
+				}
+
+				iStart = one.i
+				jStart = one.j
+				for legal(mapHeight, mapLength, iStart, jStart) {
+					uniquePoses[pos{
+						i: iStart,
+						j: jStart,
+					}] = true
+					iStart = iStart - iDis
+					jStart = jStart - jDis
+				}
+			}
+		}
+	}
+	return strconv.Itoa(len(uniquePoses))
+}
+
+func printUniqueMap(mapHeight int, mapLength int, uniquePoses map[pos]bool) {
+	for i := range mapHeight {
+		for j := range mapLength {
+			if uniquePoses[pos{
+				i: i,
+				j: j,
+			}] {
+
+				fmt.Printf("# ")
+			} else {
+
+				fmt.Printf(". ")
+			}
+		}
+		fmt.Println()
+	}
 }
