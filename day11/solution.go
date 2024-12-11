b436b2e0b2cca55276aca186fbc8dcaf3e3f040d
nyaaar <nyaaarlathotep@163.com>
2024-12-11 14:57:28 +0800

    day11part2 lol, an old method.

--- day11/solution.go
+++ day11/solution.go
@@ -43,61 +43,49 @@ func get25Times(numStrs []string, total int) int {
 
 func PartTwo(input string) string {
 	numStrs := strings.Split(input, " ")
-	m := make(map[string]int)
-	res := make([]string, 0)
-	for _, v := range numStrs {
-		vRes := get25TimesStrs([]string{v})
-		m[v] = len(vRes)
-		res = append(res, vRes...)
-	}
-
-	total := 0
-	newRes := make([]string, 0)
-	for _, v := range res {
-		if vRes, ok := m[v]; ok {
-			total = total + vRes*vRes
-			continue
-		}
-		vRes := get25TimesStrs([]string{v})
-		m[v] = len(vRes)
-		newRes = append(newRes, vRes...)
-	}
-
-	for _, v := range newRes {
-		if vRes, ok := m[v]; ok {
-			total = total + vRes
-			continue
-		}
-		vRes := get25TimesStrs([]string{v})
-		m[v] = len(vRes)
-		total += len(vRes)
-	}
-	return strconv.Itoa(total)
+	return get75TimesStrs(numStrs)
 
 }
-func get25TimesStrs(numStrs []string) []string {
-	final := make([]string, 0)
-	for _, num := range numStrs {
-		asteroids := []string{num}
-		for range 25 {
-			next := make([]string, 0)
-			slices.Grow(next, len(asteroids))
-			for _, a := range asteroids {
-				if a == "0" {
-					next = append(next, "1")
-				} else if len(a)%2 == 0 {
-					left, _ := strconv.Atoi(a[:len(a)/2])
-					right, _ := strconv.Atoi(a[len(a)/2:])
-					next = append(next, strconv.Itoa(left))
-					next = append(next, strconv.Itoa(right))
-				} else {
-					v, _ := strconv.Atoi(a)
-					next = append(next, strconv.Itoa(v*2024))
-				}
-			}
-			asteroids = next
+func get75TimesStrs(numStrs []string) string {
+	asteroids := make(map[string]int)
+	for _, a := range numStrs {
+		if v, ok := asteroids[a]; ok {
+			asteroids[a] = v + 1
+		} else {
+			asteroids[a] = 1
 		}
-		final = append(final, asteroids...)
 	}
-	return final
+	for range 75 {
+		next := make(map[string]int)
+		for k, v := range asteroids {
+			if k == "0" {
+				nextKey := "1"
+				mapAddOrSet(next, nextKey, v)
+			} else if len(k)%2 == 0 {
+				left, _ := strconv.Atoi(k[:len(k)/2])
+				right, _ := strconv.Atoi(k[len(k)/2:])
+
+				mapAddOrSet(next, strconv.Itoa(left), v)
+				mapAddOrSet(next, strconv.Itoa(right), v)
+			} else {
+				num, _ := strconv.Atoi(k)
+				mapAddOrSet(next, strconv.Itoa(num*2024), v)
+			}
+		}
+		asteroids = next
+	}
+	total := 0
+	for _, v := range asteroids {
+		total += v
+
+	}
+	return strconv.Itoa(total)
+}
+
+func mapAddOrSet(next map[string]int, nextKey string, v int) {
+	if nextV, ok := next[nextKey]; ok {
+		next[nextKey] = nextV + v
+	} else {
+		next[nextKey] = v
+	}
 }
