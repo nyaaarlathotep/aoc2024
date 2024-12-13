10671537620cbe6e30f234f4e3ee8920c8833edf
nyaaar <nyaaarlathotep@163.com>
2024-12-13 16:13:31 +0800

    day13part1 dp... don't want to save any time.

---
+++ day13/solution.go
@@ -0,0 +1,112 @@
+package day13
+
+import (
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type remainXY struct {
+	x, y int
+}
+
+type tokenState struct {
+	a, b int
+}
+
+// PartOne each button would need to be pressed no more than 100 times
+func PartOne(input string) string {
+	parts := strings.Split(input, "\n\n")
+	re1 := regexp.MustCompile(`X\+(\d+), Y\+(\d+)`)
+	re2 := regexp.MustCompile(`X=(\d+), Y=(\d+)`)
+	aTokens := 3
+	bTokens := 1
+	totalMin := 0
+	for _, p := range parts {
+		var ax, ay, bx, by, rx, ry int
+		lines := strings.Split(p, "\n")
+		matches := re1.FindAllStringSubmatch(lines[0], -1)
+		for _, match := range matches {
+			ax, _ = strconv.Atoi(match[1])
+			ay, _ = strconv.Atoi(match[2])
+		}
+		matches = re1.FindAllStringSubmatch(lines[1], -1)
+		for _, match := range matches {
+			bx, _ = strconv.Atoi(match[1])
+			by, _ = strconv.Atoi(match[2])
+		}
+		matches = re2.FindAllStringSubmatch(lines[2], -1)
+		for _, match := range matches {
+			rx, _ = strconv.Atoi(match[1])
+			ry, _ = strconv.Atoi(match[2])
+		}
+
+		var getCount func(int, int)
+		succeedTokens := make([]tokenState, 0)
+		getCount = func(remainX int, remainY int) {
+			var dp [][]remainXY
+			dp = append(dp, []remainXY{{
+				x: rx,
+				y: ry,
+			}})
+			for i := 1; i <= 100; i++ {
+				remain := dp[i-1][0]
+				xy := remainXY{
+					x: remain.x - ax,
+					y: remain.y - ay,
+				}
+				if xy.x == 0 && xy.y == 0 {
+					succeedTokens = append(succeedTokens, tokenState{
+						a: i,
+						b: 0,
+					})
+					break
+				}
+				if xy.x <= 0 || xy.y <= 0 {
+					break
+				}
+				dp = append(dp, []remainXY{xy})
+			}
+
+			for j := 0; j < len(dp); j++ {
+				for i := 1; i <= 100; i++ {
+					remain := dp[j][i-1]
+					xy := remainXY{
+						x: remain.x - bx,
+						y: remain.y - by,
+					}
+					if xy.x == 0 && xy.y == 0 {
+						succeedTokens = append(succeedTokens, tokenState{
+							a: j,
+							b: i,
+						})
+						break
+					}
+					if xy.x <= 0 || xy.y <= 0 {
+						break
+					}
+					dp[j] = append(dp[j], xy)
+				}
+			}
+
+		}
+		getCount(rx, ry)
+		minTokens := math.MaxInt
+		for _, t := range succeedTokens {
+			totalTokens := t.a*aTokens + t.b*bTokens
+			if totalTokens < minTokens {
+				minTokens = totalTokens
+			}
+		}
+		if minTokens != math.MaxInt {
+			totalMin += minTokens
+		}
+	}
+	return strconv.Itoa(totalMin)
+}
+
+func PartTwo(input string) string {
+	return ""
+
+}
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"aoc2024/day12"
+	"aoc2024/day13"
 	"fmt"
 	"os"
 )
 
 func main() {
-	content, err := os.ReadFile("./day12/input")
+	content, err := os.ReadFile("./day13/input")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("partOne res: %s \n", day12.PartOne(string(content)))
-	fmt.Printf("partTwo res: %s \n", day12.PartTwo(string(content)))
+	fmt.Printf("partOne res: %s \n", day13.PartOne(string(content)))
+	fmt.Printf("partTwo res: %s \n", day13.PartTwo(string(content)))
 }
