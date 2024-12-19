ed9195052fa396e13a47f761b19a947c4ca483ec
nyaaar <nyaaarlathotep@163.com>
2024-12-19 15:22:37 +0800

    day19part2 man! the fastest one.

--- day19/solution.go
+++ day19/solution.go
@@ -37,6 +37,32 @@ func exist(towel string, towels *map[string]bool) bool {
 }
 
 func PartTwo(input string) string {
-	return ""
+	parts := strings.Split(input, "\n\n")
+	towels := make(map[string]bool)
+	for _, towel := range strings.Split(parts[0], ", ") {
+		towels[towel] = true
+	}
+	count := 0
+	for _, t := range strings.Split(parts[1], "\n") {
+		count = count + poss(t, towels, make(map[string]int))
+	}
+	return strconv.Itoa(count)
 
+}
+func poss(towel string, towels map[string]bool, cache map[string]int) int {
+	if len(towel) == 0 {
+		return 1
+	}
+	if v, ok := cache[towel]; ok {
+		return v
+	}
+	count := 0
+	for k := range towels {
+		if strings.HasPrefix(towel, k) {
+			remainCount := poss(towel[len(k):], towels, cache)
+			count = count + remainCount
+		}
+	}
+	cache[towel] = count
+	return count
 }
